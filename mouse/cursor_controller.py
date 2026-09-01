50de4db75439aa261f82bce6ed41a7c660f34b60
Shreya Shrikant Barge <shreyabarge01@gmail.com>
2026-09-01 20:25:29 +0530

    Add smoothing to cursor movement

--- mouse/cursor_controller.py
+++ mouse/cursor_controller.py
@@ -1,5 +1,6 @@
 # mouse/cursor_controller.py
 
+from collections import deque
 import pyautogui
 
 pyautogui.FAILSAFE = True  # move mouse to top-left corner to abort
@@ -9,9 +10,16 @@ SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
 
 
 class CursorController:
-    def __init__(self):
+    def __init__(self, smoothing_window=5):
+        """
+        smoothing_window: how many recent positions to average over.
+        Higher = smoother but more lag. Lower = snappier but more jitter.
+        """
         self.screen_width = SCREEN_WIDTH
         self.screen_height = SCREEN_HEIGHT
+        self.smoothing_window = smoothing_window
+        self.recent_x = deque(maxlen=smoothing_window)
+        self.recent_y = deque(maxlen=smoothing_window)
 
     def move_to(self, norm_x, norm_y):
         """
@@ -22,8 +30,16 @@ class CursorController:
         norm_x = max(0.0, min(1.0, norm_x))
         norm_y = max(0.0, min(1.0, norm_y))
 
-        screen_x = int(norm_x * self.screen_width)
-        screen_y = int(norm_y * self.screen_height)
+        # Add to rolling history
+        self.recent_x.append(norm_x)
+        self.recent_y.append(norm_y)
+
+        # Average over recent frames for smoothing
+        smooth_x = sum(self.recent_x) / len(self.recent_x)
+        smooth_y = sum(self.recent_y) / len(self.recent_y)
+
+        screen_x = int(smooth_x * self.screen_width)
+        screen_y = int(smooth_y * self.screen_height)
 
         try:
             pyautogui.moveTo(screen_x, screen_y)
